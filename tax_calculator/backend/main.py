9803d877acd13ec7b8c8b583b8ecd2f9618882d0
Zohaib Sattar <zabizubi86@gmail.com>
2026-05-15 14:08:50 +0500

    Update main.py

--- tax_calculator/backend/main.py
+++ tax_calculator/backend/main.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "main:app",
-        host="full-stack-web-development-real-world-projects-production.up.railway.app",
+        host=0.0.0.0,
         port=8000,
         reload=True
     )
